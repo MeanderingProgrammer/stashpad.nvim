2876c0a049de7a9d7ffa185b416ab01017919ebe
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-04-27 15:17:10 -0700

    minor edit to unit test

--- tests/state_spec.lua
+++ tests/state_spec.lua
@@ -2,10 +2,10 @@
 
 local Eq = assert.are.same
 
----@param opts stashpad.UserConfig
+---@param config stashpad.UserConfig
 ---@return string[]
-local function validate(opts)
-    require('stashpad').setup(opts)
+local function validate(config)
+    require('stashpad').setup(config)
     return require('stashpad.state').validate()
 end
 
@@ -15,53 +15,50 @@ describe('state', function()
     end)
 
     it('extra', function()
-        Eq(
-            {
-                'additional - expected: nil, got: boolean',
-                'file.additional - expected: nil, got: boolean',
-            },
-            validate({
-                additional = true,
-                file = { additional = true },
-            })
-        )
+        local config = {
+            additional = true,
+            file = { additional = true },
+        }
+        local expected = {
+            'additional - expected: nil, got: boolean',
+            'file.additional - expected: nil, got: boolean',
+        }
+        Eq(expected, validate(config))
     end)
 
     it('type', function()
-        Eq(
-            {
-                'file - expected: table, got: string',
-                'git.branch - expected: function, got: string',
-                'project.markers - expected: string[], got: string',
-                'project.order - expected: ("remote"|"root"|"lsp"|function)[], got: string',
-                'win.border - expected: (string|string[]), got: false',
+        local config = {
+            file = 'invalid',
+            git = {
+                branch = 'invalid',
             },
-            ---@diagnostic disable: assign-type-mismatch
-            validate({
-                file = 'invalid',
-                git = {
-                    branch = 'invalid',
-                },
-                project = {
-                    markers = 'invalid',
-                    order = 'invalid',
-                },
-                win = {
-                    border = false,
-                },
-            })
-        )
+            project = {
+                markers = 'invalid',
+                order = 'invalid',
+            },
+            win = {
+                border = false,
+            },
+        }
+        local expected = {
+            'file - expected: table, got: string',
+            'git.branch - expected: function, got: string',
+            'project.markers - expected: string[], got: string',
+            'project.order - expected: ("remote"|"root"|"lsp"|function)[], got: string',
+            'win.border - expected: (string|string[]), got: false',
+        }
+        Eq(expected, validate(config))
+    end)
 
-        Eq(
-            {
-                'project.order.1 - expected: ("remote"|"root"|"lsp"|function), got: "invalid"',
+    it('value', function()
+        local config = {
+            project = {
+                order = { 'invalid' },
             },
-            ---@diagnostic disable: assign-type-mismatch
-            validate({
-                project = {
-                    order = { 'invalid' },
-                },
-            })
-        )
+        }
+        local expected = {
+            'project.order.1 - expected: ("remote"|"root"|"lsp"|function), got: "invalid"',
+        }
+        Eq(expected, validate(config))
     end)
 end)
